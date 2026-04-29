c51c70d6baf78f14ea2c9a385dd9b60fe7772375
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-04-29 15:09:05 -0500

    movie.py

--- movie.py
+++ movie.py
@@ -7,6 +7,8 @@ class Movie:
         self.year = year
 # FREEZE CODE END
     # TODO: Define the __str__ method!
+    def __str__(self):
+        return f'Movie: {self.title} (Directed by {self.director}, {self.year})'
 
 
 # FREEZE CODE BEGIN
@@ -17,6 +19,9 @@ if __name__ == "__main__":
     year = input("Enter the release year: ")
 # FREEZE CODE END
     
-    
+
     # TODO: Construct a Movie object!
     # TODO: Print the object!
+
+my_movie = Movie(title, director, year)
+print(my_movie)

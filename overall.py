031b441bd19978c5a6b91d69f8bb8ad2befe4b20
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-04-29 15:33:42 -0500

    car.py

--- overall.py
+++ overall.py
@@ -1,4 +1,5 @@
 # TODO: Import modules
+import car_utils, car
 def main():
     cars = {}  # Dictionary to store cars with car_id as key and car objects as values
 
@@ -15,23 +16,29 @@ def main():
         if choice == '1':
           """TODO: Call the appropriate function from utils.py to create 
           the car, add it to the dictionary, and print the car."""
+          my_car = car_utils.create_car_from_input()
+          print(my_car)
+          print("Car added.")
 
 
         elif choice == '2':
           """TODO: Call the appropriate function from utils.py to display
           all the cars in the cars dictionary."""
+          car_utils.display_cars(cars)
 
         elif choice == '3':
           car_id = input("Enter the car ID to drive:\n")
           miles = float(input("How many miles to drive?\n"))
           """TODO: Look up the car in the dictionary, call the appropriate
           class method to increase the mileage of the car, and print the car."""
-          
+          car.drive(miles)
+
         elif choice == '4':
           car_id = input("Enter the car ID to paint:\n")
           new_color = input("Enter the new color:\n")
           """TODO: Look up the car in the dictionary, call the appropriate
           class method to change the color of the car, and print the car."""
+          car.change_color(new_color)
 
         elif choice == '5':
             print("Goodbye!")

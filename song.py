6acc0f1b8299993193c288082952741a7d6c65cf
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-04-29 15:03:06 -0500

    song.py

--- song.py
+++ song.py
@@ -1,1 +1,10 @@
 # Write your code here!
+class Song:
+    def __init__(self, name, artist, length):
+        self.name = name
+        self.artist = artist
+        self.length = length
+    def get_length_in_seconds(self):
+        cancion_en_minutos = self.length
+        cancion_en_segundos = cancion_en_minutos * 60
+        return cancion_en_segundos

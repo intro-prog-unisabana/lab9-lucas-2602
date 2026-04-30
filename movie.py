8e3316dc4de765d977d0e821a72096f7a9069a33
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-04-30 10:09:49 -0500

    movie.py

--- movie.py
+++ movie.py
@@ -18,10 +18,8 @@ if __name__ == "__main__":
     director = input("Enter the director's name: ")
     year = input("Enter the release year: ")
 # FREEZE CODE END
-    
+    my_movie = Movie(title, director, year)
+    print(my_movie)
 
     # TODO: Construct a Movie object!
     # TODO: Print the object!
-
-my_movie = Movie(title, director, year)
-print(my_movie)

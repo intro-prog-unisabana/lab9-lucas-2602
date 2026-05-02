5100ed06362a5d895c3e5070e8be2cd28eaa703e
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-05-02 17:24:09 -0500

    car.py

--- bank_account.py
+++ bank_account.py
@@ -1,1 +1,14 @@
 # bank_account.py
+def __init__(self, account_number, balance=0.0):
+    self.account_number = account_number
+    self.balance = balance
+def deposit(self, amount):
+    self.balance = self.balance + amount
+def withdraw(self, amount):
+    if amount > self.balance:
+        return -1
+    else:
+        self.balance = self.balance - amount
+        return 0
+def __str__(self):
+    return f'Account Number: **{self.account_number[-2:]}\nCurrent Balance: {self.balance}'

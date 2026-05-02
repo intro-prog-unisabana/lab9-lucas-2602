0acc49234b368c55463d20914c207c78d1952104
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-05-02 17:28:38 -0500

    bank_account.py

--- bank_account.py
+++ bank_account.py
@@ -1,14 +1,18 @@
 # bank_account.py
-def __init__(self, account_number, balance=0.0):
-    self.account_number = account_number
-    self.balance = balance
-def deposit(self, amount):
-    self.balance = self.balance + amount
-def withdraw(self, amount):
-    if amount > self.balance:
-        return -1
-    else:
-        self.balance = self.balance - amount
-        return 0
-def __str__(self):
-    return f'Account Number: **{self.account_number[-2:]}\nCurrent Balance: {self.balance}'
+class BankAccount:
+    def __init__(self, account_number, balance=0.0):
+        self.account_number = account_number
+        self.balance = balance
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+    def withdraw(self, amount):
+        if amount > self.balance:
+            return -1
+        else:
+            self.balance = self.balance - amount
+            return 0
+    def __str__(self):
+        return f'Account Number: **{str(self.account_number)[-2:]}\nCurrent Balance: {self.balance:.2f}'
+
+account = BankAccount(1234, 100.0)
+print(account)

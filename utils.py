fb1a13e001301b67cca8258c18dd8e99291d9a14
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-05-03 09:49:18 -0500

    utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,5 @@
 # utils.py
+def person_data():
+    nombre = input("Enter the person's name:\n")
+    numero_de_cuenta = input("Enter a 4-digit account number:\n")
+    saldo_inicial = ("Enter the initial balance:\n")

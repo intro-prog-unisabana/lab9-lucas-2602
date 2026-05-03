d2d7739cc48323e1ef4119757c8197fbc5f5e9c9
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-05-03 12:44:04 -0500

    aircraft_altitude

--- aircraft_altitude.py
+++ aircraft_altitude.py
@@ -1,2 +1,18 @@
 from aircraft import Aircraft
-modelo = input("Enter aircraft model:\n")
+modelo = input("Enter aircraft model: ")
+maquina = Aircraft(modelo)
+while True:
+    comando = input("Enter command (A for ascent, D for descent, X to exit): ")
+    if comando.upper() == "X":
+        break
+    parts = comando.split()
+    altitud = parts[0]
+    unidades = int(parts[1])
+
+    if altitud.upper() == "A":
+        maquina.climb(unidades)
+
+    elif altitud.upper() == "D":
+        maquina.descend(unidades)
+
+print(f"Final altitude: {maquina.altitude} feet")
